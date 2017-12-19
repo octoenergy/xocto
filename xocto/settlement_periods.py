c9725e560f78d86035e84bf6586e9d870f56f55c
David Winterbottom <david.winterbottom@gmail.com>
2017-12-19 10:52:50 +0000

    Rework settlement calculations
    To be a bit easier to understand.

--- xocto/settlement_periods.py
+++ xocto/settlement_periods.py
@@ -3,7 +3,7 @@ import datetime
 import pytz
 
 
-def to_timezone(value, timezone):
+def _to_timezone(value, timezone):
     """
     Converts an aware datetime.datetime to the given time.
     """
@@ -14,61 +14,45 @@ def to_timezone(value, timezone):
     return value
 
 
-def is_dst(timezone):
-    now = pytz.utc.localize(datetime.datetime.utcnow())
-    return now.astimezone(timezone).dst() != datetime.timedelta(0)
-
-
-def convert_sp_and_date_to_utc(sp, date):
+def convert_sp_and_date_to_utc(sp, date, timezone="Europe/London"):
     """
-    Convert a SP (Settlement Period) and Date combination into a UTC date.
+    Return the datetime for the start of a given settlement period.
+
     We assume the time of the first SP period of the day is 00:00 local time.
-    :param sp: Integer representing the settlement period
-    :param date: date/datetime object
+
+    :param sp: Integer representing the settlement period (between 1 and 48)
+    :param date: date object
+    :param string: The local timezone of the date
     :return: UTC representation of the given SP and Date
     """
-    # We reduce 1 from the SP so we can multiply
-    # it properly by 30, the half-hour period.
-    # The First SP of the day is 00:00.
-    sp -= 1
-    minutes = sp * 30
-
-    london_timezone = pytz.timezone('Europe/London')
-    london_midnight = london_timezone.localize(
+    local_midnight = pytz.timezone(timezone).localize(
         datetime.datetime(date.year, date.month, date.day, hour=0, minute=0, second=0))
-
-    utc_start_of_day = to_timezone(london_midnight, pytz.UTC)
-    utc_time = utc_start_of_day + datetime.timedelta(minutes=minutes)
-    return utc_time
+    return _to_timezone(local_midnight, pytz.UTC) + datetime.timedelta(minutes=(sp - 1) * 30)
 
 
-def convert_utc_to_sp_and_date(utc_datetime):
+def convert_utc_to_sp_and_date(utc_datetime, timezone="Europe/London"):
     """
-    Convert a timezone-naive datetime object, in UTC,
-    to a SP (Settlement Period) and Date combination.
+    Convert a timezone-naive datetime object, in UTC, to a SP (Settlement Period) and Date
+    combination.
+
     :param utc_datetime: datetime timezone-aware object in UTC
     """
-    """
-    Convert a datetime object, in UTC,
-    to a SP (Settlement Period) and Date combination.
-    """
-
+    # Flatten the minutes to the nearest half hour
     if utc_datetime.minute < 30:
         minutes = 0
     else:
         minutes = 30
-
     utc_datetime = utc_datetime.replace(minute=minutes)
 
-    london_timezone = pytz.timezone('Europe/London')
-    london_datetime = to_timezone(utc_datetime, london_timezone)
-    # We re-create the midnight object so the DST settings are correct
-    d = datetime.datetime(london_datetime.year, london_datetime.month, london_datetime.day)
-    london_midnight = london_timezone.localize(d, london_timezone)
+    # Convert dt to localtime
+    local_timezone = pytz.timezone(timezone)
+    local_dt = _to_timezone(utc_datetime, local_timezone)
 
-    delta = london_datetime - london_midnight
+    # We re-create the midnight object so the DST settings are correct
+    d = datetime.datetime(local_dt.year, local_dt.month, local_dt.day)
+    local_midnight = local_timezone.localize(d, local_timezone)
+
+    delta = local_dt - local_midnight
     minutes_delta = (delta.seconds // 60) + 30
     sp = minutes_delta // 30
-
-    date = utc_datetime.astimezone(london_timezone)
-    return sp, date.date()
+    return sp, local_dt.date()
