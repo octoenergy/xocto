0bb43ee48238cbcea371aae1afb9728a1ea22a72
Omer Korner <omer.korner@octoenergy.com>
2022-04-14 13:56:21 +0100

    Reformat files with new black version

--- setup.py
+++ setup.py
@@ -27,7 +27,12 @@ setup(
         "Programming Language :: Python :: 3.6",
     ],
     packages=find_packages(exclude=["tests"]),
-    install_requires=["pytz==2022.1", "django==3.2.13", "structlog==20.2.0", "python-dateutil==2.8.2"],
+    install_requires=[
+        "pytz==2022.1",
+        "django==3.2.13",
+        "structlog==20.2.0",
+        "python-dateutil==2.8.2",
+    ],
     extras_require={
         "dev": ["wheel==0.29.0", "twine==1.8.1", "black==22.3.0", "isort==4.3.21"],
         "test": [
