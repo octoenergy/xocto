bf7058a54f4dd4d67cb481cafb49d7fd59693974
James Beith <james@beith.co.uk>
2022-05-03 14:26:34 +1000

    Support for Django v4

--- tests/settings.py
+++ tests/settings.py
@@ -1,5 +1,6 @@
 SECRET_KEY = "x"
 DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3"}}
 INSTALLED_APPS = ["xocto", "django.contrib.auth", "django.contrib.contenttypes"]
+USE_DEPRECATED_PYTZ = True
 USE_TZ = True
 TIME_ZONE = "Europe/London"
