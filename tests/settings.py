71259427390454cc010b9d03471e67eaf0c87edb
noelia.mari <noelia.mari@octoenergy.com>
2022-12-20 17:22:42 +0100

    Add tests for storage module
    This commit introduces tests that check overall functionality of storage module. Also, a new setting has been introduced so the tests can run properly.

--- tests/settings.py
+++ tests/settings.py
@@ -4,3 +4,5 @@ INSTALLED_APPS = ["xocto", "django.contrib.auth", "django.contrib.contenttypes"]
 USE_DEPRECATED_PYTZ = True
 USE_TZ = True
 TIME_ZONE = "Europe/London"
+STORAGE_BACKEND = "xocto.storage.storage.MemoryFileStore"
+RAISE_ERROR_ON_EXISTING_S3_KEYS = False
