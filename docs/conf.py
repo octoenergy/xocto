4dec4b66b69284cd180b13060ea5fd4d99317569
Daniel Roy Greenfeld <daniel.roy.greenfeld@octoenergy.com>
2023-01-11 11:49:01 -0800

    Conf.py re-org

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@
 # sys.path.insert(0, os.path.abspath('.'))
 
 
-# TODO: Use with autodoc 
+# TODO: Use with autodoc
 # from django.conf import settings
 
 # settings.configure()
@@ -36,11 +36,7 @@ release = "2.2.1"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-    "myst_parser",
-    "sphinx.ext.autodoc",
-    'sphinx.ext.autosummary'
-    ]
+extensions = ["myst_parser", "sphinx.ext.autodoc", "sphinx.ext.autosummary"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
