481dc581b760e11a169ace7b0977ecfb07172651
James Beith <james@beith.co.uk>
2023-04-04 11:42:39 +1000

    Bump xocto version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 REPO_ROOT = path.abspath(path.dirname(__file__))
 
-VERSION = "3.2.0"
+VERSION = "4.0b0"
 
 with open(path.join(REPO_ROOT, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
