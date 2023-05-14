db1878516f8b263dbc49d7e94d9aed2ad89880d8
Philip Tysoe <philtysoe@gmail.com>
2023-05-14 18:33:18 +0700

    Assert function return values inline

--- tests/test_tracing.py
+++ tests/test_tracing.py
@@ -8,40 +8,36 @@ from xocto import tracing
 def test_set_tags_single_string_tag(mocker):
     span = ddtrace.Span(name="active-span")
     mocker.patch("xocto.tracing.ddtrace.tracer.current_span", return_value=span)
-    return_value = tracing.set_tags({"foo": "bar"})
+    assert tracing.set_tags({"foo": "bar"}) is None
     assert span.get_tag("foo") == "bar"
-    assert return_value is None
 
 
 def test_set_tags_single_bytes_tag(mocker):
     span = ddtrace.Span(name="active-span")
     mocker.patch("xocto.tracing.ddtrace.tracer.current_span", return_value=span)
-    return_value = tracing.set_tags({"foo": b"bar"})
+    assert tracing.set_tags({"foo": b"bar"}) is None
 
     # bytes cast to str in Python 3, unicode in Python 2 (Python 2 not tested)
     # see https://ddtrace.readthedocs.io/en/stable/api.html#ddtrace.Span.set_tag_str
     assert span.get_tag("foo") == str(b"bar")
-    assert return_value is None
 
 
 def test_set_tags_multiple_string_tags(mocker):
     span = ddtrace.Span(name="active-span")
     mocker.patch("xocto.tracing.ddtrace.tracer.current_span", return_value=span)
     tags = {"foo": "bar", "animal": "cat"}
-    return_value = tracing.set_tags(tags)
+    assert tracing.set_tags(tags) is None
     assert span.get_tag("foo") == "bar"
     assert span.get_tag("animal") == "cat"
-    assert return_value is None
 
 
 def test_set_tags_multiple_mixed_type_tags(mocker):
     span = ddtrace.Span(name="active-span")
     mocker.patch("xocto.tracing.ddtrace.tracer.current_span", return_value=span)
     tags = {"foo": "bar", "animal": b"cat"}
-    return_value = tracing.set_tags(tags)
+    assert tracing.set_tags(tags) is None
     assert span.get_tag("foo") == "bar"
     assert span.get_tag("animal") == str(b"cat")
-    assert return_value is None
 
 
 # set_tag
@@ -50,17 +46,15 @@ def test_set_tags_multiple_mixed_type_tags(mocker):
 def test_set_tag_single_string_tag(mocker):
     span = ddtrace.Span(name="active-span")
     mocker.patch("xocto.tracing.ddtrace.tracer.current_span", return_value=span)
-    return_value = tracing.set_tag(key="foo", value="bar")
+    assert tracing.set_tag(key="foo", value="bar") is None
     assert span.get_tag("foo") == "bar"
-    assert return_value is None
 
 
 def test_set_tag_single_bytes_tag(mocker):
     span = ddtrace.Span(name="active-span")
     mocker.patch("xocto.tracing.ddtrace.tracer.current_span", return_value=span)
-    return_value = tracing.set_tag(key="foo", value=b"bar")
+    assert tracing.set_tag(key="foo", value=b"bar") is None
     assert span.get_tag("foo") == str(b"bar")
-    assert return_value is None
 
 
 # set_global_tag
@@ -69,6 +63,5 @@ def test_set_tag_single_bytes_tag(mocker):
 def test_set_global_tag(mocker):
     span = ddtrace.Span(name="root-span")
     mocker.patch("xocto.tracing.ddtrace.tracer.current_root_span", return_value=span)
-    return_value = tracing.set_global_tag(key="foo", value="bar")
+    assert tracing.set_global_tag(key="foo", value="bar") is None
     assert span.get_tag("foo") == "bar"
-    assert return_value is None
