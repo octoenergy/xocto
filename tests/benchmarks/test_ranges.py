8413debd4d5bdadf08f995425af248cd3e680e60
Peter Byfield <peter.byfield@kraken.tech>
2025-01-21 16:25:43 +0100

    Add benchmarks for FiniteDatetimeRange intersection and union

--- tests/benchmarks/test_ranges.py
+++ tests/benchmarks/test_ranges.py
@@ -1,3 +1,4 @@
+import datetime
 import random
 from decimal import Decimal as D
 
@@ -21,3 +22,67 @@ def test_any_gaps(benchmark):
     ranges_ = _shuffled([ranges.Range(D(i), D(i + 1)) for i in range(1000)])
     any_overlapping = benchmark(ranges.any_gaps, ranges_)
     assert any_overlapping is False
+
+
+class TestFiniteDatetimeRange:
+    def test_intersection_is_none(self, benchmark):
+        r1 = ranges.FiniteDatetimeRange(
+            datetime.datetime(2020, 1, 1),
+            datetime.datetime(2020, 1, 2),
+        )
+        r2 = ranges.FiniteDatetimeRange(
+            datetime.datetime(2020, 1, 3),
+            datetime.datetime(2020, 1, 4),
+        )
+
+        result = benchmark(lambda: r2 & r1)
+
+        assert result is None
+
+    def test_intersection_is_not_none(self, benchmark):
+        r1 = ranges.FiniteDatetimeRange(
+            datetime.datetime(2020, 1, 1),
+            datetime.datetime(2020, 1, 3),
+        )
+        r2 = ranges.FiniteDatetimeRange(
+            datetime.datetime(2020, 1, 2),
+            datetime.datetime(2020, 1, 4),
+        )
+
+        result = benchmark(lambda: r2 & r1)
+
+        assert result == ranges.FiniteDatetimeRange(
+            datetime.datetime(2020, 1, 2),
+            datetime.datetime(2020, 1, 3),
+        )
+
+    def test_union_is_none(self, benchmark):
+        r1 = ranges.FiniteDatetimeRange(
+            datetime.datetime(2020, 1, 1),
+            datetime.datetime(2020, 1, 2),
+        )
+        r2 = ranges.FiniteDatetimeRange(
+            datetime.datetime(2020, 1, 3),
+            datetime.datetime(2020, 1, 4),
+        )
+
+        result = benchmark(lambda: r2 | r1)
+
+        assert result is None
+
+    def test_union_is_not_none(self, benchmark):
+        r1 = ranges.FiniteDatetimeRange(
+            datetime.datetime(2020, 1, 1),
+            datetime.datetime(2020, 1, 3),
+        )
+        r2 = ranges.FiniteDatetimeRange(
+            datetime.datetime(2020, 1, 2),
+            datetime.datetime(2020, 1, 4),
+        )
+
+        result = benchmark(lambda: r2 | r1)
+
+        assert result == ranges.FiniteDatetimeRange(
+            datetime.datetime(2020, 1, 1),
+            datetime.datetime(2020, 1, 4),
+        )
