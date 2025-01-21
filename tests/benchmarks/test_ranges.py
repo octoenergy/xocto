0652f77820c1a72fd5dcfccf0a55ec0c66a1fe2f
Peter Byfield <peter.byfield@kraken.tech>
2025-01-21 16:27:31 +0100

    Group benchmarks by function

--- tests/benchmarks/test_ranges.py
+++ tests/benchmarks/test_ranges.py
@@ -1,8 +1,6 @@
 import random
 from decimal import Decimal as D
 
-import pytest
-
 from xocto import ranges
 
 
@@ -13,14 +11,12 @@ def _shuffled(ranges_, *, seed=42):
     return ranges_
 
 
-@pytest.mark.benchmark(group="ranges.any_overlapping")
 def test_any_overlapping(benchmark):
     ranges_ = _shuffled([ranges.Range(D(i), D(i + 1)) for i in range(1000)])
     any_overlapping = benchmark(ranges.any_overlapping, ranges_)
     assert any_overlapping is False
 
 
-@pytest.mark.benchmark(group="ranges.any_gaps")
 def test_any_gaps(benchmark):
     ranges_ = _shuffled([ranges.Range(D(i), D(i + 1)) for i in range(1000)])
     any_overlapping = benchmark(ranges.any_gaps, ranges_)
